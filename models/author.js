bde438334a93d7a4551a5d79df322c4e6ca1380e
daxas-boop <daxas.swd@hotmail.com>
2021-01-14 21:20:04 -0300

    initial commit

---
+++ models/author.js
@@ -0,0 +1,48 @@
+var mongoose = require('mongoose');
+const { DateTime } = require('luxon');
+const { concat } = require('async');
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema({
+  first_name: { type: String, required: true, maxlength: 100 },
+  family_name: { type: String, required: true, maxlength: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date },
+});
+
+// Virtual for author's full name
+AuthorSchema.virtual('name').get(function () {
+  return this.family_name + ', ' + this.first_name;
+});
+
+// Virtual for author's lifespan
+AuthorSchema.virtual('lifespan').get(function () {
+  const dateBirth = this.date_of_birth
+    ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
+    : ' ';
+  const dateDeath = this.date_of_death
+    ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+    : ' ';
+  return dateBirth.concat(' - ', dateDeath);
+});
+
+// Virtual for author's URL
+AuthorSchema.virtual('url').get(function () {
+  return '/catalog/author/' + this._id;
+});
+
+AuthorSchema.virtual('date_of_birth_formatted').get(function () {
+  return this.date_of_birth
+    ? this.date_of_birth.toISOString().split('T')[0]
+    : '';
+});
+
+AuthorSchema.virtual('date_of_death_formatted').get(function () {
+  return this.date_of_death
+    ? this.date_of_death.toISOString().split('T')[0]
+    : '';
+});
+
+//Export model
+module.exports = mongoose.model('Author', AuthorSchema);
